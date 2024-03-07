203d9e2439ea5aa86ac1fee085992335ad2e041d
giangkevin <test@ubuntu.myguest.virtualbox.org>
2024-03-07 19:08:53 +0100

    Add login.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->group('auth', function(RouteCollection $routes){
+    $routes->match(['get', 'post'], 'login', 'Auth::login');
+    $routes->match(['get','post'], 'register', 'Auth::register');
+});
---
+++ app/Controllers/Auth.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+class Auth extends BaseController
+{
+    public function login(): string
+    {
+        return view('auth/login');
+    }
+}
+
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
